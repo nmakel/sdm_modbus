c5d9602625433cd740c87acc8bdd7ea95e0cd90e
niels <51261230+nmakel@users.noreply.github.com>
2020-04-25 17:35:01 +0200

    updated pymodbus requirement to 2.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sdm_modbus",
-    version="0.0.1",
+    version="0.1.0",
     description="Eastron SDM Modbus parser library",
     long_description=long_description,
     long_description_content_type="text/markdown",
