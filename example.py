f92119d9409c0ff7e3b7b064e3753c83ae65f98e
niels <51261230+nmakel@users.noreply.github.com>
2020-04-25 17:35:14 +0200

    new minimal example script

---
+++ example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+import sdm_modbus
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("host", type=str, help="ModbusTCP address")
+    argparser.add_argument("port", type=int, help="ModbusTCP port")
+    argparser.add_argument("--unit", type=int, default=1, help="Modbus unit")
+    args = argparser.parse_args()
+
+    meter = sdm_modbus.SDM120(host=args.host, port=args.port, unit=args.unit)
+    meter.pprint()
