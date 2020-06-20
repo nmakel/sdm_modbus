19d562f694cd81f6f5097efb3219806fe0d79697
niels <51261230+nmakel@users.noreply.github.com>
2020-06-20 12:41:01 +0200

    added register batch reading

--- sdm_modbus/__init__.py
+++ sdm_modbus/__init__.py
@@ -138,7 +138,7 @@ class SDM:
             raise
 
     def _read(self, value):
-        address, length, rtype, dtype, vtype, label, fmt = value
+        address, length, rtype, dtype, vtype, label, fmt, batch = value
 
         try:
             if rtype == registerType.INPUT:
@@ -149,6 +149,55 @@ class SDM:
                 raise NotImplementedError(rtype)
         except NotImplementedError:
             raise
+
+    def _read_all(self, values, rtype):
+        addr_min = False
+        addr_max = False
+        addr_type = False
+
+        for k, v in values.items():
+            v_addr = v[0]
+            v_length = v[1]
+
+            if addr_min is False:
+                addr_min = v_addr
+            if addr_max is False:
+                addr_max = v_addr + v_length
+
+            if v_addr < addr_min:
+                addr_min = v_addr
+            if (v_addr + v_length) > addr_max:
+                addr_max = v_addr + v_length
+
+        results = {}
+        offset = addr_min
+        length = addr_max - addr_min
+
+        try:
+            if rtype == registerType.HOLDING:
+                data = self._read_holding_registers(offset, length)
+            elif rtype == registerType.INPUT:
+                data = self._read_input_registers(offset, length)
+            else:
+                raise NotImplementedError(rtype)
+
+            for k, v in values.items():
+                address, length, rtype, dtype, vtype, label, fmt, batch = v
+
+                if address > offset:
+                    skip_bytes = address - offset
+                    offset += skip_bytes
+                    data.skip_bytes(skip_bytes * 2)
+
+                results[k] = self._decode_value(data, length, dtype, vtype)
+                offset += length
+
+        except NotImplementedError:
+            raise
+        except AttributeError:
+            return results
+
+        return results
 
     def _write(self, value, data):
         address, length, rtype, dtype, vtype, label, fmt = value
@@ -176,11 +225,19 @@ class SDM:
 
         return self._write(self.registers[key], data)
 
-    def read_all(self, rtype=False):
-        if rtype:
-            return {k: self.read(k) for k, v in self.registers.items() if (v[2] == rtype)}
-        else:
-            return {k: self.read(k) for k, v in self.registers.items()}
+    def read_all(self, rtype=registerType.INPUT):
+        registers = {k: v for k, v in self.registers.items() if (v[2] == rtype)}
+        results = {}
+
+        for batch in range(1, len(registers)):
+            register_batch = {k: v for k, v in registers.items() if (v[7] == batch)}
+
+            if not register_batch:
+                break
+
+            results.update(self._read_all(register_batch, rtype))
+
+        return results
 
 
 class SDM120(SDM):
@@ -192,47 +249,47 @@ class SDM120(SDM):
         super().__init__(*args, **kwargs)
 
         self.registers = {
-            "voltage": (0x0000, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Voltage", "V"),
-            "current": (0x0006, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Current", "A"),
-            "power_active": (0x000c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Active)", "W"),
-            "power_apparent": (0x0012, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Apparent)", "VA"),
-            "power_reactive": (0x0018, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Reactive)", "VAr"),
-            "power_factor": (0x001e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power Factor", ""),
-            "phase_angle": (0x0024, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Phase Angle", "°"),
-            "frequency": (0x0046, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Frequency", "Hz"),
-            "import_energy_active": (0x0048, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Imported Energy (Active)", "kWh"),
-            "export_energy_active": (0x004a, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Imported Energy (Active)", "kWh"),
-            "import_energy_reactive": (0x004c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Imported Energy (Reactive)", "kVArh"),
-            "export_energy_reactive": (0x004e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Exported Energy (Reactive)", "kVArh"),
-            "total_demand_power_active": (0x0054, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Demand Power (Active)", "W"),
-            "maximum_total_demand_power_active": (0x0056, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Total Demand Power (Active)", "W"),
-            "import_demand_power_active": (0x0058, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Import Demand Power (Active)", "W"),
-            "maximum_import_demand_power_active": (0x005a, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Import Demand Power (Active)", "W"),
-            "export_demand_power_active": (0x005c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Export Demand Power (Active)", "W"),
-            "maximum_export_demand_power_active": (0x005e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Export Demand Power (Active)", "W"),
-            "total_demand_current": (0x0102, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Demand Current", "A"),
-            "maximum_total_demand_current": (0x0108, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Total Demand Current", "A"),
-            "total_energy_active": (0x0156, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Energy (Active)", "kWh"),
-            "total_energy_reactive": (0x0158, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Energy (Reactive)", "kVArh"),
+            "voltage": (0x0000, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Voltage", "V", 1),
+            "current": (0x0006, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Current", "A", 1),
+            "power_active": (0x000c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Active)", "W", 1),
+            "power_apparent": (0x0012, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Apparent)", "VA", 1),
+            "power_reactive": (0x0018, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power (Reactive)", "VAr", 1),
+            "power_factor": (0x001e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Power Factor", "", 1),
+            "phase_angle": (0x0024, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Phase Angle", "°", 1),
+            "frequency": (0x0046, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Frequency", "Hz", 1),
+            "import_energy_active": (0x0048, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Imported Energy (Active)", "kWh", 1),
+            "export_energy_active": (0x004a, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Exported Energy (Active)", "kWh", 1),
+            "import_energy_reactive": (0x004c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Imported Energy (Reactive)", "kVArh", 1),
+            "export_energy_reactive": (0x004e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Exported Energy (Reactive)", "kVArh", 1),
+            "total_demand_power_active": (0x0054, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Demand Power (Active)", "W", 2),
+            "maximum_total_demand_power_active": (0x0056, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Total Demand Power (Active)", "W", 2),
+            "import_demand_power_active": (0x0058, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Import Demand Power (Active)", "W", 2),
+            "maximum_import_demand_power_active": (0x005a, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Import Demand Power (Active)", "W", 2),
+            "export_demand_power_active": (0x005c, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Export Demand Power (Active)", "W", 2),
+            "maximum_export_demand_power_active": (0x005e, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Export Demand Power (Active)", "W", 2),
+            "total_demand_current": (0x0102, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Demand Current", "A", 3),
+            "maximum_total_demand_current": (0x0108, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Maximum Total Demand Current", "A", 3),
+            "total_energy_active": (0x0156, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Energy (Active)", "kWh", 4),
+            "total_energy_reactive": (0x0158, 2, registerType.INPUT, registerDataType.FLOAT32, float, "Total Energy (Reactive)", "kVArh", 4),
 
-            "demand_time": (0x0000, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Demand Time", "s"),
-            "demand_period": (0x0002, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Demand Period", "s"),
-            "relay_pulse_width": (0x000c, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Relay Pulse Width", "ms"),
+            "demand_time": (0x0000, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Demand Time", "s", 1),
+            "demand_period": (0x0002, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Demand Period", "s", 1),
+            "relay_pulse_width": (0x000c, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Relay Pulse Width", "ms", 1),
             "network_parity_stop": (0x0012, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Network Parity Stop", [
-                "N-1", "E-1", "O-1", "N-2"]),
-            "meter_id": (0x0014, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Meter ID", ""),
+                "N-1", "E-1", "O-1", "N-2"], 1),
+            "meter_id": (0x0014, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Meter ID", "", 1),
             "baud": (0x001c, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Baud Rate", [
-                2400, 4800, 9600, -1, -1, 1200]),
+                2400, 4800, 9600, -1, -1, 1200], 1),
             "p1_output_mode": (0x0056, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "P1 Output Mode", [
                 0x0, "Import Energy (Active)", "Import + Export Energy (Active)", 0x3, "Export Energy (Active)",
-                "Import Energy (Reactive)", "Import + Export Energy (Reactive)", 0x7, "Export Energy (Reactive)"]),
-            "display_scroll_timing": (0xf900, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Display Scroll Timing", "s"),
+                "Import Energy (Reactive)", "Import + Export Energy (Reactive)", 0x7, "Export Energy (Reactive)"], 1),
+            "display_scroll_timing": (0xf900, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Display Scroll Timing", "s", 2),
             "p1_divisor": (0xf910, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "P1 Divisor", [
-                "0.001kWh/imp", "0.01kWh/imp", "0.1kWh/imp", "1kWh/imp"]),
+                "0.001kWh/imp", "0.01kWh/imp", "0.1kWh/imp", "1kWh/imp"], 2),
             "measurement_mode": (0xf920, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Measurement Mode", [
-                0x0, "Total Imported", "Total Imported + Exported", "Total Imported - Exported"]),
+                0x0, "Total Imported", "Total Imported + Exported", "Total Imported - Exported"], 2),
             "indicator_mode": (0xf930, 2, registerType.HOLDING, registerDataType.FLOAT32, int, "Pulse/LED Indicator Mode", [
-                "Import + Export Energy (Active)", "Import Energy (Active)", "Export Energy (Active)"])
+                "Import + Export Energy (Active)", "Import Energy (Active)", "Export Energy (Active)"], 2)
 
         }
 
