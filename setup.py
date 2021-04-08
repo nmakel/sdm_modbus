755f413fce81d9e02c1b30c6ba5248a4d8c04e4a
niels <51261230+nmakel@users.noreply.github.com>
2021-04-08 21:04:07 +0200

    move to more standard project structure

--- setup.py
+++ setup.py
@@ -2,32 +2,4 @@
 
 import setuptools
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="sdm_modbus",
-    version="0.4.4",
-    description="Eastron SDM Modbus parser library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    author="nmakel",
-    author_email="",
-    url="https://github.com/nmakel/sdm_modbus",
-    packages=["sdm_modbus"],
-    include_package_data=True,
-    install_requires=[
-        "pymodbus>=2.3.0"
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-        "Natural Language :: English",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License"
-    ]
-)
+setuptools.setup()
