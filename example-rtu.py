04f39622699156218f33a07592c176c9be65fcc6
niels <51261230+nmakel@users.noreply.github.com>
2021-07-25 20:01:09 +0200

    revert example-rtu to sdm120

--- example-rtu.py
+++ example-rtu.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     argparser.add_argument("--json", action="store_true", default=False, help="Output as JSON")
     args = argparser.parse_args()
 
-    meter = sdm_modbus.SDM72(
+    meter = sdm_modbus.SDM120(
         device=args.device,
         stopbits=args.stopbits,
         parity=args.parity,
