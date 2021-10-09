a53d1cf7807cc6e7048eac97551ed167b727ae27
niels <51261230+nmakel@users.noreply.github.com>
2021-10-09 18:51:41 +0200

    update example files to use scaling

--- example-rtu.py
+++ example-rtu.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     )
 
     if args.json:
-        print(json.dumps(meter.read_all(), indent=4))
+        print(json.dumps(meter.read_all(scaling=True), indent=4))
     else:
         print(f"{meter}:")
         print("\nInput Registers:")
 
         for k, v in meter.read_all(sdm_modbus.registerType.INPUT).items():
-            address, length, rtype, dtype, vtype, label, fmt, batch = meter.registers[k]
+            address, length, rtype, dtype, vtype, label, fmt, batch, sf = meter.registers[k]
 
             if type(fmt) is list or type(fmt) is dict:
                 print(f"\t{label}: {fmt[str(v)]}")
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         print("\nHolding Registers:")
 
         for k, v in meter.read_all(sdm_modbus.registerType.HOLDING).items():
-            address, length, rtype, dtype, vtype, label, fmt, batch = meter.registers[k]
+            address, length, rtype, dtype, vtype, label, fmt, batch, sf = meter.registers[k]
 
             if type(fmt) is list:
                 print(f"\t{label}: {fmt[v]}")
