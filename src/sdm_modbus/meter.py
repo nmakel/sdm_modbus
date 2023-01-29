595e02a5eb0ba676867d978e0515eb14f8ae8ee0
Kristof Rado <rado.krisi@gmail.com>
2023-01-29 21:37:38 +0100

    Remove explicit unit paramter when writing holding registers

--- src/sdm_modbus/meter.py
+++ src/sdm_modbus/meter.py
@@ -215,7 +215,7 @@ class Meter:
         return None
 
     def _write_holding_register(self, address, value):
-        return self.client.write_registers(address=address, values=value, unit=self.unit)
+        return self.client.write_registers(address=address, values=value)
 
     def _encode_value(self, data, dtype):
         builder = BinaryPayloadBuilder(byteorder=self.byteorder, wordorder=self.wordorder)
